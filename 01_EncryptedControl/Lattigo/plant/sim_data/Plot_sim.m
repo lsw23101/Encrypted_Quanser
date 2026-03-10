ab562dbc13d92839c18659a07610ea040561c1de
unknown <lsw@cdslst.kr>
2026-03-10 15:33:57 +0900

    comments

--- 01_EncryptedControl/Lattigo/plant/sim_data/Plot_sim.m
+++ 01_EncryptedControl/Lattigo/plant/sim_data/Plot_sim.m
@@ -1,22 +1,14 @@
-%% 1. 초기화 및 데이터 로드
 clear; clc; close all;
 
-% CSV 파일 읽기 (헤더가 있는 경우 readtable이 편리합니다)
 filename = 'plant_data_python.csv';
-
-if ~isfile(filename)
-    error('파일을 찾을 수 없습니다: %s \n파이썬 시뮬레이션을 먼저 실행해주세요.', filename);
-end
 
 data = readtable(filename);
 
-% 데이터 추출
 iter = data.iter;
 y0 = data.y0;
 y1 = data.y1;
 u0 = data.u0;
 
-%% 2. 그래프 그리기
 figure('Color', 'w', 'Position', [100, 100, 800, 600]);
 
 % --- (1) System Outputs (y0, y1) ---
@@ -41,7 +33,4 @@ xlabel('Iteration', 'FontSize', 12);
 ylabel('Input Value');
 xlim([0, max(iter)]);
 
-% 전체 제목
 sgtitle('FHE Control Simulation Result (SIMO)', 'FontSize', 16, 'FontWeight', 'bold');
-
-fprintf('>> 그래프 작성 완료.\n');
